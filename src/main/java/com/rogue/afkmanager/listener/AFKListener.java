029bf7bf83bcdcec478c8da6cac223558d6c4dd1
Spencer Alderman <spam.mehere@hotmail.com>
2013-04-27 20:15:48 -0400

    Implemented Listener

--- src/main/java/com/rogue/afkmanager/listener/AFKListener.java
+++ src/main/java/com/rogue/afkmanager/listener/AFKListener.java
@@ -16,12 +16,54 @@
  */
 package com.rogue.afkmanager.listener;
 
+import com.rogue.afkmanager.AFKManager;
+import com.rogue.afkmanager.player.LMPlayer;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
 /**
  *
  * @since 0.1
  * @author 1Rogue
  * @version 0.1
  */
-public class AFKListener {
+public class AFKListener implements Listener {
+    
+    protected final AFKManager plugin;
+    
+    public AFKListener(AFKManager main) {
+        this.plugin = main;
+    }
+    
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerActivity(PlayerMoveEvent e) {
+        
+    }
+    
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerLogout(PlayerQuitEvent e) {
+        /*if (LMPlayer.isAFK(event.getPlayer())) {
+            Location back = LMPlayer.getSavedPlace(event.getPlayer());
+            event.getPlayer().teleport(back);
+        }
+        LMPlayer.removeTime(event.getPlayer());
+        LMPlayer.removeAFK(event.getPlayer());*/
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerKick(PlayerKickEvent e) {
+        LMPlayer player = AFKManager.getPlugin().getPlayerHandler().getPlayer(e.getPlayer().getName());
+        if (player.isAFK()) {
+            Location back = player.getSavedLocation();
+            e.getPlayer().teleport(back);
+        }
+        AFKManager.getPlugin().getPlayerHandler().remPlayer(e.getPlayer().getName());
+    }
+    
 
 }
