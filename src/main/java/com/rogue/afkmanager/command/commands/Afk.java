a9fb61295d5c1e65c9f8e00e4cb4f7afe87eee9b
Spencer Alderman <spam.mehere@hotmail.com>
2013-04-27 21:49:44 -0400

    Test round 4

--- src/main/java/com/rogue/afkmanager/command/commands/Afk.java
+++ src/main/java/com/rogue/afkmanager/command/commands/Afk.java
@@ -27,7 +27,7 @@ import org.bukkit.command.CommandSender;
 public class Afk implements LMCommand {
 
     public boolean execute(CommandSender sender, String[] args) {
-        // test again
+        // test once more
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
