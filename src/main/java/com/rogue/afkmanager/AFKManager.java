d234dc0de0582a8943404dfa8cc0240d9bc17457
Spencer Alderman <spam.mehere@hotmail.com>
2013-04-27 20:16:02 -0400

    Cleaned up main class

--- src/main/java/com/rogue/afkmanager/AFKManager.java
+++ src/main/java/com/rogue/afkmanager/AFKManager.java
@@ -16,7 +16,10 @@
  */
 package com.rogue.afkmanager;
 
+import com.rogue.afkmanager.command.CommandHandler;
 import com.rogue.afkmanager.configuration.Configuration;
+import com.rogue.afkmanager.listener.AFKListener;
+import com.rogue.afkmanager.player.PlayerHandler;
 import com.rogue.afkmanager.runnable.UpdateRunnable;
 import java.util.logging.Level;
 import org.bukkit.Bukkit;
@@ -33,6 +36,9 @@ public class AFKManager extends JavaPlugin {
 
     private PermManager perms;
     private Configuration configs;
+    private AFKListener listener;
+    protected PlayerHandler phandle;
+    private CommandHandler chandle;
     
     /**
      * Loads config files and checks for updates on server loading.
@@ -42,23 +48,37 @@ public class AFKManager extends JavaPlugin {
      */
     @Override
     public void onLoad() {
+        this.getLogger().log(Level.INFO, "Loading configs");
         configs = new Configuration();
         configs.loadDefaults();
-
+        
+        this.getLogger().log(Level.INFO, "Running update check");
+        this.getLogger().log(Level.INFO, "This can be disabled in your config.yml");
         if (configs.getBoolean("update-check")) {
             Bukkit.getScheduler().runTaskLater(this, new UpdateRunnable(), 1);
         }
     }
 
     /**
-     * Creates an instance of the permissions manager for AFKManager.
+     * Creates an instance of the permissions manager and listener.
      * 
      * @since 0.1
      * @version 0.1
      */
     @Override
     public void onEnable() {
+        this.getLogger().log(Level.INFO, "Enabling Listener");
+        listener = new AFKListener(this);
+        Bukkit.getPluginManager().registerEvents(listener, this);
+        
+        this.getLogger().log(Level.INFO, "Enabling Permissions");
         perms = new PermManager(this.getServer().getPluginManager().getPlugin("Vault").isEnabled());
+        
+        this.getLogger().log(Level.INFO, "Enabling Player Handler");
+        phandle = new PlayerHandler();
+        
+        this.getLogger().log(Level.INFO, "Enabling Command Handler");
+        chandle = new CommandHandler();
         this.getLogger().log(Level.INFO, "{0} is enabled!", this.getName());
     }
     
@@ -82,8 +102,8 @@ public class AFKManager extends JavaPlugin {
      * 
      * @return The plugin instance
      */
-    public static Plugin getPlugin() {
-        return Bukkit.getServer().getPluginManager().getPlugin("SayFixer");
+    public static AFKManager getPlugin() {
+        return (AFKManager)Bukkit.getServer().getPluginManager().getPlugin("AFKManager");
     }
     
     /**
@@ -96,5 +116,41 @@ public class AFKManager extends JavaPlugin {
      */
     public PermManager getPermManager() {
         return perms;
+    }
+    
+    /**
+     * Gets the instance of the listener.
+     * 
+     * @since 0.1
+     * @version 0.1
+     * 
+     * @return AFKManager's listener
+     */
+    public AFKListener getListener() {
+        return listener;
+    }
+    
+    /**
+     * Gets the instance of the player handler.
+     * 
+     * @since 0.1
+     * @version 0.1
+     * 
+     * @return AFKManager's player handler
+     */
+    public PlayerHandler getPlayerHandler() {
+        return phandle;
+    }
+    
+    /**
+     * Gets the instance of the command handler.
+     * 
+     * @since 0.1
+     * @version 0.1
+     * 
+     * @return AFKManager's command handler
+     */
+    public CommandHandler getCommandHandler() {
+        return chandle;
     }
 }
